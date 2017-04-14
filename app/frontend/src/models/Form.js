61bcc381f02b32641238277f0e10c5cf502fbadd
tnraptor14 <tnraptor14@hotmail.com>
2017-04-14 18:40:06 -0500

    basic frontend setup

---
+++ app/frontend/src/models/Form.js
@@ -0,0 +1,52 @@
+import Errors from './Errors';
+import axios from 'axios';
+
+export default class Form {
+    constructor(data) {
+        this.originalData = data;
+        for (let field in data) {
+            this[field] = data[field];
+        }
+        this.errors = new Errors();
+    }
+
+    getData() {
+        let data = Object.assign({}, this);
+        delete data.originalData;
+        delete data.errors;
+        return data;
+    }
+
+    submit(url, params) {
+        let data = this.getData();
+        if (params) {
+            data = {params: params, data: data};
+        }
+        return new Promise((resolve, reject) => {
+            axios.post(url, data)
+                .then(response => {
+                    this.onSuccess(response.data);
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.data);
+                    reject(error.data);
+                });
+        });
+    }
+
+    onSuccess(data) {
+        this.reset();
+    }
+
+    onFail(errors) {
+        this.errors.record(errors);
+    }
+
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+        this.errors.clear();
+    }
+}
---
+++ app/frontend/src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <form>
+        <input type="text" placeholder="Enter your username" required>
+        <select required>
+
+        </select>
+        <button type="submit">Join Game</button>
+    </form>
+</template>
+
+<script>
+
+    export default {
+        data() {
+
+        }
+    }
+
+</script>
